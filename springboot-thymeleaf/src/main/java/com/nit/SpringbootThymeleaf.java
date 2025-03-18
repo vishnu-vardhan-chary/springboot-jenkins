7445f9da6d3b3c9514e735575a421d994bdcd835
vishnu-vardhan-chary <imvishnuchary@gmail.com>
2025-03-18 18:26:40 +0530

    first commit

---
+++ springboot-thymeleaf/src/main/java/com/nit/SpringbootThymeleaf.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleaf {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleaf.class, args);
+	}
+
+}
